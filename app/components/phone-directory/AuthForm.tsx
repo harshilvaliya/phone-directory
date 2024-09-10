9aff6c2c6d6e9a3fb3a9d9be749d50533965733a
Harshil Valiya <harshilvaliya40@gmail.com>
2024-09-10 15:08:53 +0530

    pass manage

--- app/components/phone-directory/AuthForm.tsx
+++ app/components/phone-directory/AuthForm.tsx
@@ -15,7 +15,7 @@ export default function AuthForm({ onAuthenticate }: AuthFormProps) {
   const [error, setError] = useState("");
 
   // Access the password from the environment variable
-  const correctPassword = process.env.NEXT_PUBLIC_AUTH_PASSWORD;
+  const correctPassword = process.env.PASSWORD;
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
